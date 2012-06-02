e81b6e0b70f0490269c26ad3256648e36ea3d167
Zain Memon <zain@inzain.net>
2012-06-02 12:38:03 -0700

    Initial repo structure

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='logs',
+    version='0.0.1',
+    description='Logging for humans.',
+    long_description=readme,
+    author='Zain Memon',
+    author_email='zain@inzain.net',
+    url='https://github.com/zain/logs',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
